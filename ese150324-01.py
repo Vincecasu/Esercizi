bd92eb0fb12e957ca9e9f8e5c956289902a77257
vincecasu <vincenzocasuccio77@gmail.com>
2024-03-16 10:37:28 +0100

    Esercizi_Vari

---
+++ ese150324-01.py
@@ -0,0 +1,41 @@
+"""for i in [[4,5,6],[5,6,7]]:
+    print (i)"""
+
+import random
+
+"""
+l1 = []
+for i in range (5):
+    l1.append(random.randrange(10))
+print(l1)
+          
+for v in (l1):
+    print (v)
+"""
+l1 = []
+"""
+for v in range (7):
+    print (v)
+"""
+"""
+for v in range(len(l1)):
+    print (l1[v])
+"""
+'''
+t = 23
+for i in range (10):
+    print(t,end=" ")
+    t = t + 23
+print()
+'''
+numeri = []
+
+for i in range(10):
+   numeri.append(random.random())
+
+print (sum(numeri))
+somma = 0
+for v in numeri:
+   somma += v
+print(somma)
+
