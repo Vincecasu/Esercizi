847d5283c1aeca4792f5d9a0b2b22039d8cf5ee6
vincecasu <vincenzocasuccio77@gmail.com>
2024-03-21 11:17:03 +0100

    DaSvolgere..

---
+++ >CalciatorePiùGiovane.py
@@ -0,0 +1,20 @@
+# 6) Trovare qual'è il calciatore più giovane che ha partecipato alla coppa del mondo
+
+import json
+#Per leggere un file json:
+
+filejson = open("all-world-cup-players.json","r")
+worldcup =json.load(filejson)
+filejson.close()
+
+listaAnno = []
+
+calciatorepiùgiovane = []
+
+for c in worldcup:
+    listaAnno.append(int(c["DateOfBirth"]))
+print(listaAnno)
+   
+    
+
+    
